429a6b87976cfd57a432f20a7bb42f955ef6aef9
Ali Rafat <coderrafat@gmail.com>
2023-08-06 15:14:46 +0600

    add ToogleMark

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,75 +1,71 @@
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
-import { AddTodo } from "../redux/slice/todoSlice.js";
+import { AddTodo } from "../redux/slice/todoSlice";
 import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const CreateTodo = () => {
   const dispatch = useDispatch();
   const taskInput = useRef();
-  const handleClick = () => {
-    if (!taskInput.current.value) {
-      toast.error("Enter the Task", {
+
+  const handleButton = () => {
+    if (!taskInput) {
+      toast.error("Please enter your task.", {
         position: "top-right",
-        autoClose: 3000,
+        autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
+        pauseOnHover: true,
+        draggable: true,
         progress: undefined,
         theme: "dark",
       });
     } else {
       dispatch(AddTodo(taskInput.current.value));
 
-      taskInput.current.value = " ";
+      taskInput.current.value = "";
 
       toast.success("New task has been added!", {
         position: "top-right",
-        autoClose: 3000,
+        autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
+        pauseOnHover: true,
+        draggable: true,
         progress: undefined,
         theme: "dark",
       });
-      taskInput.current.value = "";
     }
   };
+
   return (
-    <div>
-      <div className="container mt-3">
-        <div className="row">
-          <div className="col-10">
-            <input
-              type="text"
-              placeholder="Enter todo"
-              className="form-control"
-              ref={taskInput}
-            />
-          </div>
-          <div className="col-2">
-            <button className="btn btn-primary" onClick={handleClick}>
-              Add Todo
-            </button>
-            <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-            />
-            {/* Same as */}
-            <ToastContainer />
-          </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-10">
+          <input
+            ref={taskInput}
+            placeholder="Enter Your Task...."
+            className="form-control"
+            type="text"
+          />
+        </div>
+        <div className="col-md-2">
+          <button onClick={() => handleButton()} className="btn btn-primary">
+            Add Todo
+          </button>
         </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 };
